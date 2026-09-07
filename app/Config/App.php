f977b2ec36ef691de87c50141aaec15bb896818a
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-07 10:04:49 +0700

    config: set application base URL to port 6060

--- app/Config/App.php
+++ app/Config/App.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://localhost:6060/';
     public array $allowedHostnames = [];
     public string $indexPage = '';
     public string $uriProtocol = 'REQUEST_URI';
