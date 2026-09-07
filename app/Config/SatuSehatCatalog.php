cb0fb466694c3893bc9cc0e5606111315bf5d49c
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-07 17:49:05 +0700

    feat: add SATUSEHAT Postman catalog configuration

---
+++ app/Config/SatuSehatCatalog.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class SatuSehatCatalog extends BaseConfig
+{
+    public array $resources = [
+        'Patient','Practitioner','Organization','Location','Encounter','EpisodeOfCare','Condition','AllergyIntolerance',
+        'FamilyMemberHistory','Observation','Procedure','ClinicalImpression','Goal','CarePlan','ServiceRequest',
+        'Specimen','DiagnosticReport','ImagingStudy','Medication','MedicationRequest','MedicationDispense',
+        'MedicationAdministration','MedicationStatement','QuestionnaireResponse','RiskAssessment','NutritionOrder',
+        'Coverage','Account','ChargeItem','Invoice','Task','Composition','Bundle'
+    ];
+
+    public array $collections = [
+        ['key'=>'00.fhir','title'=>'FHIR Resource - Contoh Penggunaan'],
+        ['key'=>'01.rawat_jalan','title'=>'Pelayanan - Rawat Jalan'],
+        ['key'=>'02.rawat_inap','title'=>'Pelayanan - Rawat Inap'],
+        ['key'=>'03.igd','title'=>'Pelayanan - IGD'],
+        ['key'=>'04.farmasi','title'=>'Pelayanan - Farmasi'],
+        ['key'=>'06.anc','title'=>'Use Case - Antenatal Care (ANC)'],
+        ['key'=>'07.inc','title'=>'Use Case - Intranatal Care (INC)'],
+        ['key'=>'08.pnc','title'=>'Use Case - Postnatal Care (PNC)'],
+        ['key'=>'09.neonatus','title'=>'Use Case - Neonatus'],
+        ['key'=>'10.shk','title'=>'Use Case - Skrining Hipotiroid Kongenital (SHK)'],
+        ['key'=>'11.kematian','title'=>'Use Case - Kematian Maternal dan Perinatal'],
+        ['key'=>'12.mtbs','title'=>'Use Case - MTBS Prioritas'],
+        ['key'=>'13.imunisasi','title'=>'Use Case - Imunisasi'],
+        ['key'=>'14.gizi','title'=>'Use Case - Gizi'],
+        ['key'=>'15.tumbuh_kembang','title'=>'Use Case - Tumbuh Kembang'],
+        ['key'=>'16.pkpr','title'=>'Use Case - Pelayanan Kesehatan Peduli Remaja (PKPR) Luar Gedung'],
+        ['key'=>'18.imunisasi_covid','title'=>'Use Case - Imunisasi Covid 19'],
+        ['key'=>'19.ptm','title'=>'Use Case - Skrining PTM'],
+        ['key'=>'20.kanker','title'=>'Use Case - Registrasi Kanker'],
+        ['key'=>'21.jantung','title'=>'Use Case - Registrasi Jantung'],
+        ['key'=>'22.stroke','title'=>'Use Case - Registrasi Stroke'],
+        ['key'=>'23.uronefrologi','title'=>'Use Case - Registrasi Uronefrologi'],
+        ['key'=>'24.gigi','title'=>'Use Case - Gigi'],
+        ['key'=>'25.claim_swasta','title'=>'Use Case - Modul Klaim (Asuransi Swasta)'],
+        ['key'=>'26.claim_bpjs','title'=>'Use Case - Modul Klaim (BPJS-K)'],
+        ['key'=>'30.rujukan','title'=>'Use Case - Rujukan Pasien'],
+        ['key'=>'31.mata','title'=>'Use Case - Registrasi Mata'],
+        ['key'=>'32.telinga','title'=>'Use Case - Registrasi Telinga'],
+        ['key'=>'34.tb','title'=>'Use Case - Tuberkulosis'],
+        ['key'=>'36.hiv','title'=>'Use Case - HIV'],
+        ['key'=>'42.geriatri','title'=>'Use Case - Geriatri (Kesehatan Lansia)'],
+    ];
+
+    public array $patientRequests = [
+        ['name'=>'Patient - By ID','method'=>'GET','resource'=>'Patient','query'=>'id={{patient_ihs}}'],
+        ['name'=>'Patient - By NIK','method'=>'GET','resource'=>'Patient','query'=>'identifier=https://fhir.kemkes.go.id/id/nik|{{nik}}'],
+        ['name'=>'Patient - Search Name, Birthdate, Gender','method'=>'GET','resource'=>'Patient','query'=>'name={{name}}&birthdate={{birthdate}}&gender={{gender}}'],
+        ['name'=>'Patient - Create by NIK','method'=>'POST','resource'=>'Patient'],
+    ];
+
+    public array $practitionerRequests = [
+        ['name'=>'Practitioner - By ID','method'=>'GET','resource'=>'Practitioner','query'=>'id={{practitioner_ihs}}'],
+        ['name'=>'Practitioner - By NIK','method'=>'GET','resource'=>'Practitioner','query'=>'identifier=https://fhir.kemkes.go.id/id/nik|{{nik}}'],
+        ['name'=>'Practitioner - Search Name, Birthdate, Gender','method'=>'GET','resource'=>'Practitioner','query'=>'name={{name}}&birthdate={{birthdate}}&gender={{gender}}'],
+    ];
+}
