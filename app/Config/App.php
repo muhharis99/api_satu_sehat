ca807a807930f06fa43346542a9c0b828dd0c92f
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-07 17:38:48 +0700

    fix: align base URL with CI4 dev server

--- app/Config/App.php
+++ app/Config/App.php
@@ -6,7 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
-    public string $baseURL = 'http://localhost:6060/';
+    public string $baseURL = 'http://localhost:8080/';
     public array $allowedHostnames = [];
     public string $indexPage = '';
     public string $uriProtocol = 'REQUEST_URI';
