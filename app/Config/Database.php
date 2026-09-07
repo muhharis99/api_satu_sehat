80db7ed72e03b2038bb0d984c37adf789682902e
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-07 10:05:59 +0700

    Add default CI4 Database configuration

---
+++ app/Config/Database.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Database\Config;
+
+class Database extends Config
+{
+    /**
+     * --------------------------------------------------------------------------
+     * Database Connections.
+     * --------------------------------------------------------------------------
+     *
+     * This project does not require a database connection for booting yet.
+     * Keep the default connection available so CodeIgniter services can resolve
+     * the Config\Database class without failing during application startup.
+     */
+    public array $default = [
+        'DSN'          => '',
+        'hostname'     => '127.0.0.1',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => (ENVIRONMENT !== 'production'),
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => true,
+        'foundRows'    => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
+}
