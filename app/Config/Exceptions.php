02f25c8b71502768d34831854aa934de8bbf8583
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-09-07 09:20:13 +0700

    Fix CodeIgniter 4.7.4 Exceptions configuration

--- app/Config/Exceptions.php
+++ app/Config/Exceptions.php
@@ -3,31 +3,38 @@
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Debug\ExceptionHandler;
 use CodeIgniter\Debug\ExceptionHandlerInterface;
-use CodeIgniter\Debug\Exceptions as CoreExceptions;
+use Psr\Log\LogLevel;
+use Throwable;
 
+/**
+ * Setup how the exception handler works.
+ */
 class Exceptions extends BaseConfig
 {
-    /**
-     * --------------------------------------------------------------------------
-     * LOG EXCEPTIONS?
-     * --------------------------------------------------------------------------
-     */
     public bool $log = true;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Error View Paths
-     * --------------------------------------------------------------------------
-     */
-    public array $errorViewPaths = [
-        APPPATH . 'Views/errors',
-    ];
+    /** @var list<int> */
+    public array $ignoreCodes = [404];
 
     /**
-     * --------------------------------------------------------------------------
-     * Exception Handler
-     * --------------------------------------------------------------------------
+     * Path to the directory containing the cli and html error views.
      */
-    public string $handler = CoreExceptions::class;
+    public string $errorViewPath = APPPATH . 'Views/errors';
+
+    /** @var list<string> */
+    public array $sensitiveDataInTrace = [];
+
+    public bool $logDeprecations = true;
+
+    public string $deprecationLogLevel = LogLevel::WARNING;
+
+    /**
+     * Return the exception handler used by CodeIgniter.
+     */
+    public function handler(int $statusCode, Throwable $exception): ExceptionHandlerInterface
+    {
+        return new ExceptionHandler($this);
+    }
 }
